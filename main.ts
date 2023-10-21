9a98e7dcf5fd09c6af97c2cbd44c7335b53e6382
Prokop Lošťák <75861541+prokop321@users.noreply.github.com>
2023-10-21 14:19:31 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,73 @@
 import { useState, useEffect } from "react";
-// Define the type for the update function 
+
+// Define the type for the update function
 type updateFunction = (newValue?: any, oldValue?: any) => any;
+
 // Define the interface for reactive values
-interface IVueReactReactiveValue { _subscribe: (callback: updateFunction) => void; _unsubscribe: (callback: updateFunction) => void; }
-// Function to create a reactive reference 
-export function ref(initialValue: T): { value: T } & IVueReactReactiveValue { const [_render, _setRender] = useState(false); let updateStack: (() => any)[] = [];const registerUpdate = (callback: () => any) => { updateStack.push(callback); };const unregisterUpdate = (callback: () => any) => { updateStack = updateStack.filter(a => callback !== a); };const handler = { set(target: any, prop: string, val: any) { updateStack.forEach((f) => f(val, target[prop])); target[prop] = val; _setRender(prev => !prev); return true; }, };const proxy = new Proxy({ value: initialValue, _subscribe: registerUpdate, _unsubscribe: unregisterUpdate }, handler); return proxy; }
-// Function to create a reactive value 
-export function reactive(initialValue: T): { value: T } & IVueReactReactiveValue { const [_render, _setRender] = useState(false); let updateStack: (() => any)[] = [];const registerUpdate = (callback: () => any) => { updateStack.push(callback); };const unregisterUpdate = (callback: () => any) => { updateStack = updateStack.filter(a => callback !== a); };const handler = { set(target: any, prop: string, val: any) { updateStack.forEach((f) => f(val, target[prop])); target[prop] = val; _setRender(prev => !prev); return true; }, };const proxy = new Proxy({ value: initialValue, _subscribe: registerUpdate, _unsubscribe: unregisterUpdate }, handler); return proxy; }
+interface IVueReactReactiveValue {
+  _subscribe: (callback: updateFunction) => void;
+  _unsubscribe: (callback: updateFunction) => void;
+}
+
+// Function to create a reactive reference
+export function ref<T>(initialValue: T): { value: T } & IVueReactReactiveValue {
+  const [_render, _setRender] = useState(false);
+  let updateStack: (() => any)[] = [];
+
+  const registerUpdate = (callback: () => any) => {
+    updateStack.push(callback);
+  };
+
+  const unregisterUpdate = (callback: () => any) => {
+    updateStack = updateStack.filter(a => callback !== a);
+  };
+
+  const handler = {
+    set(target: any, prop: string, val: any) {
+      updateStack.forEach((f) => f(val, target[prop]));
+      target[prop] = val;
+      _setRender(prev => !prev);
+      return true;
+    },
+  };
+
+  const proxy = new Proxy({ value: initialValue, _subscribe: registerUpdate, _unsubscribe: unregisterUpdate }, handler);
+  return proxy;
+}
+
+// Function to create a reactive value
+export function reactive<T extends object>(initialValue: T): T & IVueReactReactiveValue {
+  const [_render, _setRender] = useState(false);
+  let updateStack: (() => any)[] = [];
+
+  const registerUpdate = (callback: () => any) => {
+    updateStack.push(callback);
+  };
+
+  const unregisterUpdate = (callback: () => any) => {
+    updateStack = updateStack.filter(a => callback !== a);
+  };
+
+  const handler = {
+    set(target: any, prop: string, val: any) {
+      updateStack.forEach((f) => f(val, target[prop]));
+      target[prop] = val;
+      _setRender(prev => !prev);
+      return true;
+    },
+  };
+
+  const proxy = new Proxy({ ...initialValue, _subscribe: registerUpdate, _unsubscribe: unregisterUpdate }, handler);
+  return proxy;
+}
+
 // Function to watch changes on reactive values
-export function watch(prop: { value: any } & IVueReactReactiveValue, callback: updateFunction) { useEffect(() => { prop._subscribe(callback);return () => {
-  prop._unsubscribe(callback);
-};}, [prop]); }
+function watch(prop: { value: any } & IVueReactReactiveValue, callback: updateFunction) {
+  useEffect(() => {
+    prop._subscribe(callback);
+
+    return () => {
+      prop._unsubscribe(callback);
+    };
+  }, [prop]);
+}
